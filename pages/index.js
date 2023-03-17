b3b404bac96823564e98370e14479f01dfad1b28
Roland Ding <shengxio@lanterns.fun>
2023-03-17 11:30:05 -0600

    fixed the header icon closing tag in the index page.

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Welcome to my coffee stop</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-        <i class="fa fa-coffee">
+        <i class="fa fa-coffee"></i>
       </Head>
 
       <main>
